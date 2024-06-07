a7c184d9198ba572a54e6b0f7243ca1c408c88b9
DKirill <joke@mail>
2024-06-07 11:11:42 +0300

    another

---
+++ sh/vi/lua/mylazy/dap.lua
@@ -0,0 +1,9 @@
+return {
+    'mfussenegger/nvim-dap',
+    'mfussenegger/nvim-lint',
+    'jay-babu/mason-nvim-dap.nvim',
+    'rcarriga/nvim-dap-ui',
+    'niuiic/dap-utils.nvim',
+    'nvim-telescope/telescope-dap.nvim',
+}
+
--- sh/vi/lua/mylazy/init.lua
+++ sh/vi/lua/mylazy/init.lua
@@ -74,14 +74,6 @@ return {
   'williamboman/mason.nvim', -- base --- load lang tools
   'williamboman/mason-lspconfig.nvim',
 
-  'mfussenegger/nvim-dap',
-  'mfussenegger/nvim-lint',
-  'jay-babu/mason-nvim-dap.nvim',
-  'rcarriga/nvim-dap-ui',
-  'niuiic/dap-utils.nvim',
-
-  'nvim-telescope/telescope-dap.nvim',
-
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
