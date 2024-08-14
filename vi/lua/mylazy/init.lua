d62ff2d063c4729a9c5827daf1389e62b791a12f
Diger <anon@mail>
2024-08-14 09:40:58 +0300

    another

--- vi/lua/mylazy/init.lua
+++ vi/lua/mylazy/init.lua
@@ -35,7 +35,6 @@ return {
 
   {
     'nvim-telescope/telescope.nvim',
-    -- 'nvim-telescope/telescope.nvim', branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim' },
     opts = {
       defaults = {
